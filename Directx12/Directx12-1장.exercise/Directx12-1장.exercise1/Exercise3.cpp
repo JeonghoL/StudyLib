0d2a0aff2a53d2dd0327d05ab82397aa88344237
JeongHoL <alwjw@naver.com>
2024-12-25 20:04:19 +0900

    Directx12 Commit

---
+++ Directx12/Directx12-1장.exercise/Directx12-1장.exercise1/Exercise3.cpp
@@ -0,0 +1,33 @@
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <iostream>
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+int main()
+{
+	cout.precision(8);
+
+	if (!XMVerifyCPUSupport())
+	{
+		cout << "DirectXMath를 지원하지 않음" << endl;
+		return 0;
+	}
+
+	XMVECTOR u = XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f);
+	XMVECTOR n = XMVector3Normalize(u);
+
+	float LU = XMVectorGetX(XMVector3Length(n));
+
+	cout << LU << endl;
+	if (LU == 1.0f)
+		cout << "길이 1" << endl;
+	else
+		cout << "길이 1 아님" << endl;
+
+	float powLU = powf(LU, 1.0e6f);
+	cout << "LU^(10^6) = " << powLU << endl;
+}
